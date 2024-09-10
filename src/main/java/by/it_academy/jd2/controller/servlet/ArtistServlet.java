e4ac9d5d02a12ff80fc6b3ff81b48b72ea965bff
Peniaz Evgenii <evgenii.peniaz@gmail.com>
2024-09-10 15:12:36 +0300

    add ddelete in servlets Genre and Artist

--- src/main/java/by/it_academy/jd2/controller/servlet/ArtistServlet.java
+++ src/main/java/by/it_academy/jd2/controller/servlet/ArtistServlet.java
@@ -10,40 +10,71 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 import static by.it_academy.jd2.util.PathUtil.ARTIST_SERVLET;
 
 @WebServlet(urlPatterns = ARTIST_SERVLET)
 public class ArtistServlet extends HttpServlet {
+    public static final String ERROR_ARTIST_NAME = "Имя артиста не должно состоять из пробелов";
+    public static final String MESSAGE_ARTIST_ADDED = "Исполнитель добавлен в систему под id = ";
+    public static final String MESSAGE_ARTIST_NOT_ADDED = "Исполнитель не добавлен";
+    public static final String MESSAGE_ARTIST_DELETED = "Исполнитель успешно удален";
+    public static final String MESSAGE_ARTIST_NOT_DELETED = "Исполнитель не был удален";
+
     public static final String ATTRIBUTE_ERROR_ARTIST = "artistErr";
     public static final String ATTRIBUTE_ADD_ARTIST = "artistAdd";
     public static final String ATTRIBUTE_LIST_ARTIST = "artist";
     public static final String PARAMETER_ARTIST = "artist";
+    public static final String PARAMETER_DELETE_ARTIST = "deleteArtist";
+    public static final String PARAMETER_DELETE_ARTIST_ERROR = "deleteArtistErr";
     public static final String JSP_NAME_ARTIST = "artist";
 
-    private static final IArtistService singerService = ServiceArtistFactory.getInstance();
+    private static final IArtistService artistService = ServiceArtistFactory.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute(ATTRIBUTE_LIST_ARTIST, singerService.getAll());
+        req.setAttribute(ATTRIBUTE_LIST_ARTIST, artistService.getAll());
         req.getRequestDispatcher(JspUtil.getPath(JSP_NAME_ARTIST)).forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String artist = req.getParameter(PARAMETER_ARTIST);
+        String deleteArtist = req.getParameter(PARAMETER_DELETE_ARTIST);
 
+        if(artist != null) {
+            addingArtist(req, artist);
+        }
+
+        deletionArtist(req, deleteArtist);
+        doGet(req, resp);
+    }
+
+    private void addingArtist(HttpServletRequest req, String artist) {
         try {
-            if(artist.isBlank()) {
-                throw new IllegalArgumentException();
+            if (artist.isBlank()) {
+                throw new IllegalArgumentException(ERROR_ARTIST_NAME);
             }
-            Long id = singerService.create(artist);
-            req.setAttribute(ATTRIBUTE_ADD_ARTIST, "Исполнитель добавлен в систему под id = " + id);
-            doGet(req, resp);
+            Long id = artistService.create(artist);
+            req.setAttribute(ATTRIBUTE_ADD_ARTIST, MESSAGE_ARTIST_ADDED + id);
+
         } catch (IllegalArgumentException e) {
-            req.setAttribute(ATTRIBUTE_ERROR_ARTIST, "Исполнитель не добавлен");
-            doGet(req, resp);
+            req.setAttribute(ATTRIBUTE_ERROR_ARTIST, MESSAGE_ARTIST_NOT_ADDED + ". " + e.getMessage());
+        }
+    }
+
+    private static void deletionArtist(HttpServletRequest req, String deleteArtist) {
+        if(deleteArtist != null) {
+            try {
+                boolean delete = artistService.delete(Long.valueOf(deleteArtist));
+                if(delete) {
+                    req.setAttribute(PARAMETER_DELETE_ARTIST, MESSAGE_ARTIST_DELETED);
+                } else {
+                    req.setAttribute(PARAMETER_DELETE_ARTIST_ERROR, MESSAGE_ARTIST_NOT_DELETED);
+                }
+            } catch (RuntimeException e) {
+                req.setAttribute(PARAMETER_DELETE_ARTIST_ERROR, MESSAGE_ARTIST_NOT_DELETED + " " + e.getMessage());
+            }
         }
     }
 }
--- src/main/java/by/it_academy/jd2/controller/servlet/GenreServlet.java
+++ src/main/java/by/it_academy/jd2/controller/servlet/GenreServlet.java
@@ -17,7 +17,14 @@ import static by.it_academy.jd2.util.PathUtil.*;
 
 @WebServlet(urlPatterns = GENRE_SERVLET)
 public class GenreServlet extends HttpServlet {
+    public static final String ERROR_GENRE_NAME = "Название жанра не должно состоять из пробелов";
+    public static final String MESSAGE_GENRE_ADDED = "Жанр добавлен в систему под id = ";
+    public static final String MESSAGE_GENRE_NOT_ADDED = "Жанр не добавлен";
+    public static final String MESSAGE_GENRE_DELETED = "Жанр успешно удален";
+    public static final String MESSAGE_GENRE_NOT_DELETED = "Жанр не был удален";
 
+    public static final String PARAMETER_DELETE_GENRE_ERROR = "deleteGenreErr";
+    public static final String PARAMETER_DELETE_GENRE = "deleteGenre";
     public static final String ATTRIBUTE_ERROR_GENRE = "janreErr";
     public static final String ATTRIBUTE_ADD_GENRE = "janreAdd";
     public static final String JSP_NAME_GENRE = "janre";
@@ -34,18 +41,46 @@ public class GenreServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String genre = req.getParameter(PARAMETER_GENRE);
+        String deleteGenre = req.getParameter(PARAMETER_DELETE_GENRE);
 
+        if(genre != null) {
+            addingGenre(req, genre);
+        }
+
+        if(deleteGenre != null) {
+            deletionGenre(req, deleteGenre);
+        }
+
+        doGet(req, resp);
+    }
+
+
+    private void addingGenre(HttpServletRequest req, String genre) {
         try {
             if(genre.isBlank()) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException(ERROR_GENRE_NAME);
             }
 
-            genreService.create(genre);
-            req.setAttribute(ATTRIBUTE_ADD_GENRE, "Жанр добавлен в систему");
-            doGet(req, resp);
+            Long id = genreService.create(genre);
+            req.setAttribute(ATTRIBUTE_ADD_GENRE, MESSAGE_GENRE_ADDED + id);
+
         } catch (IllegalArgumentException e) {
-            req.setAttribute(ATTRIBUTE_ERROR_GENRE, "Жанр не добавлен");
-            doGet(req, resp);
+            req.setAttribute(ATTRIBUTE_ERROR_GENRE, MESSAGE_GENRE_NOT_ADDED + e.getMessage());
+        }
+    }
+
+    private void deletionGenre(HttpServletRequest req, String deleteGenre) {
+        try {
+            boolean delete = genreService.delete(Long.valueOf(deleteGenre));
+
+            if (delete) {
+                req.setAttribute(PARAMETER_DELETE_GENRE, MESSAGE_GENRE_DELETED);
+            } else {
+                req.setAttribute(PARAMETER_DELETE_GENRE_ERROR, MESSAGE_GENRE_NOT_DELETED);
+            }
+
+        } catch (RuntimeException e) {
+            req.setAttribute(PARAMETER_DELETE_GENRE_ERROR, MESSAGE_GENRE_NOT_DELETED + " " + e.getMessage());
         }
     }
 }
